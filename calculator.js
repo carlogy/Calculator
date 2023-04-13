7eb40b013b08c3156c6348f2ae2ed34b9b70d7c6
Carlo Yannuzzi <carlogy@Carlos-MacBook-Pro.local>
2023-04-12 20:41:11 -0400

    updated styling, installed body-parser npm package, and post handling.

--- calculator.js
+++ calculator.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express()
 
 app.get("/", function(req, res) {
-    res.send("Hello World");
-})
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+    res.send("Thanks for sending that!");
+});
 
 app.listen(3000, function() {
     console.log("Sever started on port 3000");
-})
+});
