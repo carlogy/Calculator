1a5520ae544c90cdff8ca4ae5fbe3deadd0c4df2
Carlo Yannuzzi <carlogy@Carlos-MacBook-Pro.local>
2023-04-12 21:20:21 -0400

    added a BMI calculator route, with all functionality to calculate bmi.

--- calculator.js
+++ calculator.js
@@ -8,6 +8,19 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/bmicalculator", function(req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req, res) {
+    var height = Number(req.body.height);
+    var weight = Number(req.body.weight);    
+
+    var bmi = weight / (Math.pow(height, 2));
+    res.send("Your BMI is " + Math.floor(bmi));
+
+})
+
 app.post("/", function(req, res) {
     var num1 = Number(req.body.num1);
     var num2 = Number(req.body.num2);
